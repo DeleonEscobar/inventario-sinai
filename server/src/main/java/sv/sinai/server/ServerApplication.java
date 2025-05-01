4fe0cf5a828164dc4ed84e3438090f69e8a49d95
DeleonEscobar <rogrsdeleon506@gmail.com>
2025-05-01 17:40:17 -0600

    Primer commit

---
+++ server/src/main/java/sv/sinai/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package sv.sinai.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
