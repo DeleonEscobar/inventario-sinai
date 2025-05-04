d01662a755f76e92fb2489582e00691166686ef6
DeleonEscobar <rogrsdeleon506@gmail.com>
2025-05-04 13:18:32 -0600

    Modifado: Default TimeStamp para campos de auditoria y campo de Movimiento Creado por...

--- server/src/main/resources/sql/sinai.sql
+++ server/src/main/resources/sql/sinai.sql
@@ -1,90 +1,101 @@
-CREATE TABLE `warehouse`
-(
-    `id`            int PRIMARY KEY AUTO_INCREMENT,
-    `name`          varchar(255),
+CREATE TABLE `warehouse` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
+    `name` varchar(255),
     `contactUserId` int UNIQUE,
-    `status`        int,
-    `location`      varchar(255),
-    `createdAt`     timestamp DEFAULT null,
-    `updatedAt`     timestamp DEFAULT null
+    `status` int,
+    `location` varchar(255),
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE `user`
-(
-    `id`        int PRIMARY KEY AUTO_INCREMENT,
-    `username`  varchar(255) UNIQUE,
-    `password`  varchar(255),
-    `name`      varchar(255),
-    `dui`       varchar(255) UNIQUE,
-    `role`      int,
-    `createdAt` timestamp DEFAULT null,
-    `updatedAt` timestamp DEFAULT null
+CREATE TABLE `user` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
+    `username` varchar(255) UNIQUE,
+    `password` varchar(255),
+    `name` varchar(255),
+    `dui` varchar(255) UNIQUE,
+    `role` int,
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE `product`
-(
-    `id`        int PRIMARY KEY AUTO_INCREMENT,
-    `name`      varchar(255),
-    `createdAt` timestamp DEFAULT null,
-    `updatedAt` timestamp DEFAULT null
+CREATE TABLE `product` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
+    `name` varchar(255),
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE `batch`
-(
-    `id`             int PRIMARY KEY AUTO_INCREMENT,
-    `productId`      int,
-    `amount`         int,
-    `expirationDate` datetime  DEFAULT null,
-    `serialNumber`   varchar(255),
-    `price`          decimal(6, 2),
-    `createdAt`      timestamp DEFAULT null,
-    `updatedAt`      timestamp DEFAULT null
+CREATE TABLE `batch` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
+    `productId` int,
+    `amount` int,
+    `expirationDate` datetime DEFAULT null,
+    `serialNumber` varchar(255),
+    `price` decimal(6, 2),
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE `client`
-(
-    `id`        int PRIMARY KEY AUTO_INCREMENT,
-    `name`      varchar(255),
-    `address`   varchar(255),
-    `createdAt` timestamp DEFAULT null,
-    `updatedAt` timestamp DEFAULT null
+CREATE TABLE `client` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
+    `name` varchar(255),
+    `address` varchar(255),
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE `movement`
-(
-    `id`                int PRIMARY KEY AUTO_INCREMENT,
-    `notes`             text,
-    `type`              int,
-    `status`            int,
-    `clientId`          int,
+CREATE TABLE `movement` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
+    `notes` text,
+    `type` int,
+    `status` int,
+    `clientId` int,
     `responsibleUserId` int,
-    `createdAt`         timestamp DEFAULT null,
-    `updatedAt`         timestamp DEFAULT null
+    `createdByUserId` int,
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE `movementBatch`
-(
-    `id`         int PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `movementBatch` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
     `movementId` int,
-    `batchId`    int,
-    `createdAt`  timestamp DEFAULT null,
-    `updatedAt`  timestamp DEFAULT null
+    `batchId` int,
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-ALTER TABLE `warehouse`
-    ADD FOREIGN KEY (`contactUserId`) REFERENCES `user` (`id`);
+ALTER TABLE
+    `warehouse`
+ADD
+    FOREIGN KEY (`contactUserId`) REFERENCES `user` (`id`);
 
-ALTER TABLE `batch`
-    ADD FOREIGN KEY (`productId`) REFERENCES `product` (`id`);
+ALTER TABLE
+    `batch`
+ADD
+    FOREIGN KEY (`productId`) REFERENCES `product` (`id`);
 
-ALTER TABLE `movement`
-    ADD FOREIGN KEY (`clientId`) REFERENCES `client` (`id`);
+ALTER TABLE
+    `movement`
+ADD
+    FOREIGN KEY (`clientId`) REFERENCES `client` (`id`);
 
-ALTER TABLE `movement`
-    ADD FOREIGN KEY (`responsibleUserId`) REFERENCES `user` (`id`);
+ALTER TABLE
+    `movement`
+ADD
+    FOREIGN KEY (`responsibleUserId`) REFERENCES `user` (`id`);
 
-ALTER TABLE `movementBatch`
-    ADD FOREIGN KEY (`movementId`) REFERENCES `movement` (`id`);
+ALTER TABLE
+    `movement`
+ADD
+    FOREIGN KEY (`createdByUserId`) REFERENCES `user` (`id`);
 
-ALTER TABLE `movementBatch`
-    ADD FOREIGN KEY (`batchId`) REFERENCES `batch` (`id`);
+ALTER TABLE
+    `movementBatch`
+ADD
+    FOREIGN KEY (`movementId`) REFERENCES `movement` (`id`);
+
+ALTER TABLE
+    `movementBatch`
+ADD
+    FOREIGN KEY (`batchId`) REFERENCES `batch` (`id`);
