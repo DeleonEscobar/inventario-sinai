26c908f2da22aba0e9caab2747dacb448cb43d05
DeleonEscobar <rogrsdeleon506@gmail.com>
2025-05-04 02:13:40 -0600

    actualizar servicio de la entidad bodega para implementar el DTO bodega

--- server/src/main/java/sv/sinai/server/services/WarehouseService.java
+++ server/src/main/java/sv/sinai/server/services/WarehouseService.java
@@ -2,9 +2,11 @@ package sv.sinai.server.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sv.sinai.server.entities.User;
 import sv.sinai.server.entities.Warehouse;
+import sv.sinai.server.entities.dto.UserDTO;
+import sv.sinai.server.entities.dto.WarehouseDTO;
 import sv.sinai.server.repositories.IWarehouseRepository;
+import sv.sinai.server.services.extra.DTOConverter;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,35 +15,43 @@ import java.util.stream.Collectors;
 @Service
 public class WarehouseService {
     private final IWarehouseRepository warehouseRepository;
+    private final DTOConverter dtoConverter;
 
     @Autowired
-    public WarehouseService(IWarehouseRepository warehouseRepository) {
+    public WarehouseService(IWarehouseRepository warehouseRepository, DTOConverter dtoConverter) {
         this.warehouseRepository = warehouseRepository;
+        this.dtoConverter = dtoConverter;
     }
 
     // Get all warehouses
-    public List<Warehouse> getAllWarehouses() {
-        return warehouseRepository.findAll();
+    public List<WarehouseDTO> getAllWarehouses() {
+        List<Warehouse> warehouses = warehouseRepository.findAll();
+        return warehouses.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
     }
 
     // Get all warehouses by status
-    public List<Warehouse> getAllByStatus(Integer status) {
-        return warehouseRepository.findAllByStatus(status).orElseGet(List::of);
+    public List<WarehouseDTO> getAllByStatus(Integer status) {
+        List<Warehouse> warehouses = warehouseRepository.findAllByStatus(status);
+        return warehouses.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
     }
 
     // Get warehouse by id
-    public Optional<Warehouse> getWarehouseById(Integer id) {
-        return warehouseRepository.findById(id);
+    public Optional<WarehouseDTO> getWarehouseById(Integer id) {
+        return warehouseRepository.findById(id).map(this::convertToDTO);
     }
 
     // Get warehouse by name
-    public Optional<Warehouse> getWarehouseByName(String name) {
-        return warehouseRepository.findByName(name);
+    public Optional<WarehouseDTO> getWarehouseByName(String name) {
+        return warehouseRepository.findByName(name).map(this::convertToDTO);
     }
 
     // Get warehouse by contact user
-    public Optional<Warehouse> getWarehouseByContactUser(User user) {
-        return warehouseRepository.findByContactUser(user);
+    public Optional<WarehouseDTO> getWarehouseByContactUserId(Integer userId) {
+        return warehouseRepository.findByContactUserId(userId).map(this::convertToDTO);
     }
 
     // Create warehouse
@@ -64,5 +74,11 @@ public class WarehouseService {
     // Delete warehouse
     public void deleteWarehouse(Integer id) {
         warehouseRepository.deleteById(id);
+    }
+
+    // Metodo auxiliar
+    private WarehouseDTO convertToDTO(Warehouse warehouse) {
+        UserDTO userDTO = dtoConverter.userToDTO(warehouse.getContactUser());
+        return dtoConverter.warehouseToDTO(warehouse, userDTO);
     }
 }
