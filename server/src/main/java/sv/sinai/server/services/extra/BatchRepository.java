3e515aa889dcf1f714bcc826dab15d9c2ecac907
melara021 <miguelmelara011@gmail.com>
2025-05-19 23:48:39 -0600

    eliminar elementos no utilizados

--- server/src/main/java/sv/sinai/server/services/extra/BatchRepository.java
+++
@@ -1,7 +0,0 @@
-package sv.sinai.server.services.extra;
-
-import org.springframework.data.repository.Repository;
-import sv.sinai.server.entities.Batch;
-
-interface BatchRepository extends Repository<Batch, Integer> {
-}
--- server/src/main/java/sv/sinai/server/services/extra/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package sv.sinai.server.services.extra;
-
-import org.springframework.data.repository.Repository;
-import sv.sinai.server.entities.User;
-
-interface UserRepository extends Repository<User, Integer> {
-}
